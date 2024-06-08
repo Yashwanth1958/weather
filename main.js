350f75ff1eb9d8d15f39f897bae1e277bfc146ad
yashwanth reddy <yashwanthappidi@gmail.com>
2024-06-08 11:58:59 +0530

    weather

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var wind = document.querySelector('#wind')
 
 // Get your own free OWM API key at https://www.openweathermap.org/appid - please do not re-use mine!
 // You don't need an API key for this to work at the moment, but this will change eventually.
-apik = "3045dd712ffe6e702e3245525ac7fa38"
+apik = "be7be78e0dc888b1f3ecefacdfc0fc57"
 //kelvin to celcious
 function convertion(val){
     return (val - 273).toFixed(2)
@@ -19,7 +19,7 @@ function convertion(val){
     btn.addEventListener('click', function(){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
-         //.then(data => console.log(data))
+         .then(data => console.log(data))
         .then(data => {
             var nameval = data['name']
             var descrip = data['weather']['0']['description']
